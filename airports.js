eb00fa4aa062203f1e97f81cd06aaf1f02235089
afw821 <afw821@gmail.com>
2018-12-17 02:16:17 -0500

    I commented out some of the other scripts

--- airports.js
+++ airports.js
@@ -1,28 +1,28 @@
-const findAirport = function (cityName) {
-  const params = $.param({
-    "term": cityName,
-  });
-  return $.ajax({
+// const findAirport = function (cityName) {
+//   const params = $.param({
+//     "term": cityName,
+//   });
+//   return $.ajax({
     
-    url: `https://www.air-port-codes.com/api/v1/multi?${params}`,
-    method: "POST",
-    headers: {
-      "APC-Auth": "59fb789385",
-      "APC-Auth-Secret": "775bcf3ef390fd9"
-    }
-  });
-}
+//     url: `https://www.air-port-codes.com/api/v1/multi?${params}`,
+//     method: "POST",
+//     headers: {
+//       "APC-Auth": "59fb789385",
+//       "APC-Auth-Secret": "775bcf3ef390fd9"
+//     }
+//   });
+// }
 
-const findSrcAirport = function (srcCity) {
-  const params = $.param({
-    "term": srcCity,
-  });
-  return $.ajax({
-    url: `https://www.air-port-codes.com/api/v1/multi?${params}`,
-    method: "POST",
-    headers: {
-      "APC-Auth": "59fb789385",
-      "APC-Auth-Secret": "775bcf3ef390fd9"
-    }
-  });
-}
+// const findSrcAirport = function (srcCity) {
+//   const params = $.param({
+//     "term": srcCity,
+//   });
+//   return $.ajax({
+//     url: `https://www.air-port-codes.com/api/v1/multi?${params}`,
+//     method: "POST",
+//     headers: {
+//       "APC-Auth": "59fb789385",
+//       "APC-Auth-Secret": "775bcf3ef390fd9"
+//     }
+//   });
+// }
